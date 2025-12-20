30a5e8ef38f9104395783560771a0c795a521182
mel1van <mel1vanito@gmail.com>
2025-12-20 13:54:15 +0400

    data.py

--- data.py
+++ data.py
@@ -2,17 +2,17 @@
 
 from datetime import datetime, timedelta
 
-# Текущая дата + 3 дня для теста
+# Актуальная дата для доставки
 delivery_date = (datetime.now() + timedelta(days=3)).strftime("%Y-%m-%d")
 
 order_body = {
     "firstName": "Naruto",
-    "lastName": "Uchiha", 
+    "lastName": "Uzumaki",
     "address": "Konoha, 142 apt.",
     "metroStation": 4,
     "phone": "+7 800 355 35 35",
     "rentTime": 5,
-    "deliveryDate": delivery_date,  # Используем актуальную дату
+    "deliveryDate": delivery_date,
     "comment": "Sasuke, come back to Konoha",
     "color": ["BLACK"]
 }
