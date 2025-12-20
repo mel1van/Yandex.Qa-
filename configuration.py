61b3b9ea6ebc10c7ff1a20644618d12df5aebd10
mel1van <mel1vanito@gmail.com>
2025-12-20 13:54:37 +0400

    configuration.py

--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
 # Иван Мел, 38-я когорта — Финальный проект. Инженер по тестированию плюс
 
-URL_SERVICE = "https://c9a2c813-ecef-4389-97c6-96155b0b05ce.serverhub.praktikum-services.ru/"
+URL_SERVICE = "https://e94ba1b7-fab4-42bd-97d4-c7e80e0455a9.serverhub.praktikum-services.ru/"
 CREAT_ORDERS = "/api/v1/orders"
 
 print(f"Конфигурация загружена. Базовый URL: {URL_SERVICE}")
